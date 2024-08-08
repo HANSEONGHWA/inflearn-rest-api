db53ff3fa9946af4072a66b5e68ad58650bd7138
HANSEONGHWA <ghk9206@hanmail.net>
2024-08-08 18:55:41 +0900

    fail on unknown properteis

--- src/test/java/com/example/demo/event/EventControllerTests.java
+++ src/test/java/com/example/demo/event/EventControllerTests.java
@@ -2,6 +2,7 @@ package com.example.demo.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("create 201 & DTO 외의 값 무시")
     public void createEvent() throws Exception {
 
         Event event = Event.builder()
@@ -65,5 +67,33 @@ public class EventControllerTests {
                 .andExpect(jsonPath("free").value(Matchers.not(true)))
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
         ;
+    }
+
+    @Test
+    @DisplayName("DTO 외의 값 입력 시 BadRequest")
+    public void createEvent_BadRequest() throws Exception {
+
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 스터디 카페")
+                .free(true)
+                .offline(false)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON) // request contentType
+                        .accept(MediaTypes.HAL_JSON)// response Type
+                        .content(objectMapper.writeValueAsString(event))) // event 객체를 JSON 문자열로 바꿔 요청 본문에 넣어줌.
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 }
